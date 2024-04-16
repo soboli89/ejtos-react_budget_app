98da24d9b818eedf731a0a964fd7b8635c09abb2
soboli89 <soboli1989@gmail.com>
2024-04-16 15:45:20 -0400

    ready template

--- src/App.js
+++ src/App.js
@@ -1,69 +1,45 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//Code to import Budget.js
-import Budget from './components/Budget';
-
-// Add code to import the other components here under
-import Remaining from './components/Remaining';
-import ExpenseTotal from './components/ExpenseTotal';
-
 import { AppProvider } from './context/AppContext';
+import Budget from './components/Budget';
+import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
-import ExpenseItem from './components/ExpenseItem';
 import AllocationForm from './components/AllocationForm';
+import RemainingBudget from './components/Remaining';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                                           // Budget component
+                <div className='row mt-3'>
                     <div className='col-sm'>
                         <Budget />
                     </div>
-                        }        
-
-                        {
-                                             //Remaining component
                     <div className='col-sm'>
-                    <Remaining />
-                </div>        
-                        }        
-
-                        {
-                                            //ExpenseTotal component
+                        <RemainingBudget />
+                    </div>
                     <div className='col-sm'>
-                    <ExpenseTotal />
+                        <ExpenseTotal />
+                    </div>
                 </div>
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                            <div className='col-sm'>
-                    <ExpenseList />
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
                 </div>
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                            <div className='col-sm'>
-                    <ExpenseItem />
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>
                 </div>
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                            <div className='col-sm'>
-                    <AllocationForm />
-                </div>
-                        }        
-
-                </div>
+                
             </div>
         </AppProvider>
     );
 };
+
 export default App;
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -5,14 +5,25 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const [currency, setCurrency] = useState('');
+    //console.log(newBudget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
     }
     return (
+            
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: {currency} {budget}</span>
+<select className="custom-select" id="inputCurrencySelect01" onChange={(event) => setCurrency(event.target.value)}>
+                        <option defaultValue>Currency</option>
+                        <option value="$" currency="Dollar"> $ Dollar</option>
+                        <option value="£" currency="Pound">£ Pound</option>
+                        <option value="€" currency="Euro">€ Euro</option>
+                        <option value="₹" currency="Ruppee">₹ Ruppee</option>
+                  </select>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
+
 export default Budget;
